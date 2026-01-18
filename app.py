0edccb270f5f431a0c346dac9151085c46d09781
sahnideepak5611-design <sahnideepak5611@gmail.com>
2026-01-19 02:10:11 +0530

    Updated home message

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "AI App Live Successfully"
+def home():return "Hello Deepak, AI App is Working"
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
